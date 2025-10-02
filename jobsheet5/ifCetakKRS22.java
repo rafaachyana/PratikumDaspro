aee8cf646c4f5f98e4c43a7d00eff717629b2a40
rafaachyana <rafaachyana778@gmail.com>
2025-10-02 10:53:24 +0700

    ternary op t1

--- jobsheet5/ifCetakKRS22.java
+++ jobsheet5/ifCetakKRS22.java
@@ -6,11 +6,16 @@ public class ifCetakKRS22 {
         System.out.println("=== Cetak KRS Siakad ===");
         System.out.print("Apakah UKT sudah luna? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-        
+
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi.");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        }
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
+            }
+         // ternary operator
+        String pesan = uktLunas ? "Pembayaran UKT terverifikasi.\nSilakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.";
+        System.out.println(pesan);        
         sc.close();
     }
 }
